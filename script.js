6aecfcac395a0b24f613ff3bdb309dd75e92fd17
maratsumi <dextroopsgaming@gmail.com>
2022-05-08 09:29:23 +0800

    removed fade

--- script.js
+++ script.js
@@ -21,28 +21,20 @@ $(document).ready(async function(){
 
     async function aboutFadeElement(){
         if (homeShown) {
-            $(".inner-container-home").fadeOut(250);
             $("#home").slideUp(750);
             homeShown = false;
             setTimeout(function(){
-                $(".inner-container-about").fadeIn(650);
-                $(".inner-container-about").css("display", "flex");
                 $("#about").slideDown(750);
                 $("#about").css("display", "flex");
             }, 1000);
         } else if (worksShown) {
-            $(".inner-container-works").fadeOut(250);
             $("#works").slideUp(750);
             worksShown = false;
             setTimeout(function(){
-                $(".inner-container-about").fadeIn(650);
-                $(".inner-container-about").css("display", "flex");
                 $("#about").slideDown(750);
                 $("#about").css("display", "flex");
             }, 1000);
         } else {
-            $(".inner-container-about").fadeIn(650);
-            $(".inner-container-about").css("display", "flex");
             $("#about").slideDown(750);
             $("#about").css("display", "flex");
         }
@@ -50,28 +42,20 @@ $(document).ready(async function(){
 
     async function worksFadeElement(){
         if (aboutShown) {
-            $(".inner-container-about").fadeOut(250);
             $("#about").slideUp(750);
             aboutShown = false;
             setTimeout(function(){
-                $(".inner-container-works").fadeIn(650);
-                $(".inner-container-works").css("display", "flex");
                 $("#works").slideDown(750);
                 $("#works").css("display", "flex");
             }, 1000);
         } else if (homeShown) {
-            $(".inner-container-home").fadeOut(250);
             $("#home").slideUp(750);
             homeShown = false;
             setTimeout(function(){
-                $(".inner-container-works").fadeIn(650);
-                $(".inner-container-works").css("display", "flex");
                 $("#works").slideDown(750);
                 $("#about").css("display", "flex");
             }, 1000);
         } else {
-            $(".inner-container-works").fadeIn(650);
-            $(".inner-container-works").css("display", "flex");
             $("#works").slideDown(750);
             $("#works").css("display", "flex");
         }
@@ -79,28 +63,20 @@ $(document).ready(async function(){
 
     async function homeFadeElement(){
         if (aboutShown) {
-            $(".inner-container-about").fadeOut(250);
             $("#about").slideUp(750);
             aboutShown = false;
             setTimeout(function(){
-                $(".inner-container-home").fadeIn(650);
-                $(".inner-container-home").css("display", "flex");
                 $("#home").slideDown(750);
                 $("#home").css("display", "flex");
             }, 1000);
         } else if (worksShown) {
-            $(".inner-container-works").fadeOut(250);
             $("#works").slideUp(750);
             worksShown = false;
             setTimeout(function(){
-                $(".inner-container-home").fadeIn(650);
-                $(".inner-container-home").css("display", "flex");
                 $("#home").slideDown(750);
                 $("#home").css("display", "flex");
             }, 1000);
         } else {
-            $(".inner-container-home").fadeIn(650);
-            $(".inner-container-home").css("display", "flex");
             $("#home").slideDown(750);
             $("#home").css("display", "flex");
         }
